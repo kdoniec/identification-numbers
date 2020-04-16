6b0de81087dcb58ac815975756fb7dfa9ec5f884
Krzysztof Doniec <kdoniec@gmail.com>
2020-04-16 22:31:54 +0000

    Updates

---
+++ precache-manifest.dc9a56434462684542120217961be60a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3264edc64c58044f40d4e21fadc980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab134e4164a017ccef58",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "086e28b2a96723274163",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab134e4164a017ccef58",
+    "url": "/static/js/2.640e4419.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.640e4419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086e28b2a96723274163",
+    "url": "/static/js/main.08767d61.chunk.js"
+  },
+  {
+    "revision": "7171184e0e2e08e5b3d4",
+    "url": "/static/js/runtime-main.ae065dca.js"
+  }
+]);
