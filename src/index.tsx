59e1a46d62b981c68498947ee8db7a1294bd4412
Krzysztof Doniec <kdoniec@gmail.com>
2020-04-17 00:19:39 +0200

    Add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,68 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import NavbarTop from './NavbarTop';
+import * as serviceWorker from './serviceWorker';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+  <React.StrictMode>
+    <NavbarTop />
+
+    <main role="main">
+      <Jumbotron>
+        <Container>
+        <h1 className="display-3">Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <Row>
+           <Col md="4">
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><Button variant="secondary">View details »</Button></p>
+           </Col>
+           <Col md="4">
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><Button variant="secondary">View details »</Button></p>
+           </Col>
+           <Col md="4">
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><Button variant="secondary">View details »</Button></p>
+           </Col>
+        </Row>
+        <hr/>
+      </Container>
+    </main>
+
+    <footer>
+      <Container>
+        <p>© Company 2017-2018</p>
+      </Container>
+    </footer>
+
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
